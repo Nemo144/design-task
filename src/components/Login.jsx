b9da8c6db2b7296d2dbbcc94779727de260719c2
Nnamdi Okeke <onnamdialex@gmail.com>
2022-05-10 23:58:17 +0100

    :art: login page done!

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { AiOutlineEye } from "react-icons/ai";
+import MemopsLogo from "./logo";
 
 export const Login = () => {
   return (
-    <div className="flex flex-wrap justify-center items-center border-zinc-800 rounded mx-auto w-96 bg-zinc-800">
+    <div className="flex flex-wrap rounded mx-auto  mb-10 pb-10 w-96 bg-zinc-800">
       <div className="sm:mx-auto sm:w-full sm:max-w-md w-full mt-5">
         <h1 className=" text-sm font-light text-center text-gray-600  ">
           WELCOME BACK
@@ -16,17 +17,17 @@ export const Login = () => {
       <div className="mt-2 w-full text-sm font-light text-center p-5">
         <form>
           <label
-            className="flex flex-wrap justify-start text-gray-400"
-            for="E-mail or Username"
+            className="flex flex-wrap justify-start text-gray-300"
+            for="Email or Username"
           >
             E-mail or Username
           </label>
           <input
             id="email"
             type="email"
-            label="E-mail or Username"
+            label="Email or Username"
             placeholder="Enter your email or username"
-            className="w-full border border-gray-800 h-11 bg-zinc-700 rounded "
+            className="w-full border border-gray-800 h-10 bg-zinc-700 rounded "
           />
         </form>
       </div>
@@ -36,30 +37,38 @@ export const Login = () => {
           <div className="flex justify-between">
             <label
               for="Password"
-              className="flex flex-wrap justify-start text-gray-400"
+              className="flex flex-wrap justify-start text-gray-300"
             >
               Password
             </label>
-            <h3>Forgot password?</h3>
+            <h3 className="text-gray-300">Forgot password?</h3>
           </div>
-          <div className="flex justify-between">
+          <div className="relative flex w-full justify-between">
             <input
               type="password"
               placeholder="Enter your password"
               label="Password"
-              className="w-full border-gray-600 h-11 border bg-zinc-700 rounded justify-between "
-              img={<AiOutlineEye />}
+              className="w-full border-gray-600 h-10 border bg-zinc-700 rounded  "
             />
+            <div className="absolute mx-80 my-3 cursor-pointer">
+              <AiOutlineEye />
+            </div>
           </div>
         </form>
       </div>
 
-      <div>
+      <div className="w-full border rounded mx-5 h-10 border-blue-400 flex items-center justify-center bg-blue-400 text-white cursor-pointer">
         <button>Login Now</button>
       </div>
 
-      <div>
-        <h4>Not registered yet?Register →</h4>
+      <div className="mx-5 mb-5 text-gray-500">
+        <h4 className="flex justify-items-start">
+          Not registered yet?
+          <a href="/#" className="text-gray-300 ml-1">
+            {" "}
+            Register →
+          </a>
+        </h4>
       </div>
     </div>
   );
